a72b107d45224063f78317bbbcf2113e27659c07
Eric M. Gildersleeve <15720193+KD7CAO@users.noreply.github.com>
2026-09-11 10:11:01 -0500

    feat: register ICS 205B workspace tab

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,13 +5,22 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import { ElevationEnhancer } from "./ElevationEnhancer";
 import { FccUnitEnhancer } from "./FccUnitEnhancer";
+import { ICS205BWorkspace } from "./ICS205BWorkspace";
 import { IncidentMetadataEditor } from "./IncidentMetadataEditor";
 import { LocationSearchEnhancer } from "./LocationSearchEnhancer";
 import { MapObjectSelectionEnhancer } from "./MapObjectSelectionEnhancer";
 import { NifogResourceCategoriesEnhancer } from "./NifogResourceCategoriesEnhancer";
+import { registerWorkspaceTab } from "./workspaceTabRegistry";
 import "./mapObjectSelection.css";
 import "./NifogResourceCategories.css";
 import "./styles.css";
+
+registerWorkspaceTab({
+  id: "ics-205b",
+  label: "ICS 205B",
+  layout: "single",
+  content: <ICS205BWorkspace />,
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
